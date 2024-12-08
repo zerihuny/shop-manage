17182a46ac31315b46d2f4627197eae4abdb0415
Zerihun Taressa <34819166+zerihuny@users.noreply.github.com>
2024-12-08 13:08:34 +0300

    Update frm_mainmenu_a172423.Designer.vb
    tittle changed

--- prj_movelex_a172423/frm_mainmenu_a172423.Designer.vb
+++ prj_movelex_a172423/frm_mainmenu_a172423.Designer.vb
@@ -50,7 +50,7 @@ Partial Class frm_mainmenu_a172423
         Me.lbl_title.Name = "lbl_title"
         Me.lbl_title.Size = New System.Drawing.Size(235, 56)
         Me.lbl_title.TabIndex = 0
-        Me.lbl_title.Text = "MoveLex"
+        Me.lbl_title.Text = "Shop Managemet"
         '
         'btn_customer
         '
@@ -130,7 +130,7 @@ Partial Class frm_mainmenu_a172423
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(134, 17)
         Me.Label1.TabIndex = 6
-        Me.Label1.Text = "In Control And Chill"
+        Me.Label1.Text = "Created by: Tadele Taressa"
         '
         'PictureBox1
         '
